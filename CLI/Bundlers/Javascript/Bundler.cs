37dca485cf0dfae3f89841ed3bfe0d46b94cb1d2
c-ed <csedwards79@gmail.com>
2025-05-23 13:44:10 -0700

    Use the es module strategy

--- CLI/Bundlers/Javascript/Bundler.cs
+++ CLI/Bundlers/Javascript/Bundler.cs
@@ -1,9 +1,12 @@
+using System.Text.RegularExpressions;
+using System.Text;
+
 namespace CLI.Bundlers.Javascript;
 
-public class Bundler
+public static class Bundler
 {
-    public void Bundle(string entryFilePath)
-    {
-        
-    }
+    // All methods related to manual bundling (Bundle, ParseDependencies, ResolveDependencyPath, HandleUnsupportedDependency)
+    // have been removed. TypeScript compilation (tsc) now produces ES modules,
+    // and module loading is handled natively by the browser.
+    // This class is retained for potential future JavaScript processing tasks if any arise.
 }
